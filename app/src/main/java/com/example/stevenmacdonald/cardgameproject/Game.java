022a759832f958a1d91b39542d267b8252f6c135
steven2805 <stevenmac90@googlemail.com>
2017-07-06 21:59:24 +0100

    Random generation actually working =

--- app/src/main/java/com/example/stevenmacdonald/cardgameproject/Game.java
+++ app/src/main/java/com/example/stevenmacdonald/cardgameproject/Game.java
@@ -1,8 +1,11 @@
 package com.example.stevenmacdonald.cardgameproject;
 
+import android.util.Log;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 
 import static android.R.attr.value;
@@ -15,6 +18,7 @@ public class Game {
 
     private Player player;
     private Card[] hand;
+    int[] holding = new int[4];
 
 
     public Game() {
@@ -22,30 +26,40 @@ public class Game {
         this.hand = new Card[3];
 
 
+
     }
 
-    public void setupHands(){
-        int[] holding = new int[10];
+    public void setupHands() {
+        int[] holding = new int[4];
         Card deck = new Card();
         deck.getDeck();
         int counter = 0;
-        while(counter < 10){
-            Random rand = new Random();
-            rand.setSeed(System.currentTimeMillis());
-            int  temp = rand.nextInt(50);
+        while (counter < 4) {
+            int temp = randomNumber();
             holding[counter] = temp;
-            counter ++;
-
-
-
-
-
-
-
+            counter++;
 
 
         }
+    }
+
+        private int randomNumber() {
+            try {
+                TimeUnit.MILLISECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Random rand = new Random();
+            rand.setSeed(System.currentTimeMillis());
+            int temp = rand.nextInt(20);
+            return temp;
 
     }
+
+
+
+
+
+
 
 }
